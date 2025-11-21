a48cee3a53b1ec78f00d45665e42b6a335791ad6
Nat Osa (Neuro1Hub) <natesk.j20@gmail.com>
2025-11-21 22:25:43 +0000

    Try fixing this error: `Unhandled Runtime Error: TypeError: Cannot destr

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,17 @@
+'use client';
+
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
 
 const firebaseConfig = {
-  apiKey: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: publicRuntimeConfig.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
